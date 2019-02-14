ebdfa2998a6c5e8cfe8001e139c50887ed3f448d
gonzicz <gonzicz83@gmail.com>
2019-02-14 18:06:09 +0100

    rozwiązanie zadania 1.2

--- src/main/java/chapter1/task1_2/Main.java
+++ src/main/java/chapter1/task1_2/Main.java
@@ -1,9 +1,13 @@
 package chapter1.task1_2;
 
+/**
+ * Napisz program, który wyświetla na ekranie komputera wartości predefiniowanej stałe PI = 3,14
+ * Należy ptzyjąc format wyświetlania tej stałęj z dokłądnością do pięciu miejsc po przecinku
+ */
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("tast1");
+        System.out.printf("Stał PI z dokładnością do pięciu miejsc po przcinku zwynosi %.5f", Math.PI);
     }
 }
