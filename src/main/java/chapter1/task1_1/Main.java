1997b8a434e864d0c58b6b2e66d905150258b220
gonzicz <gonzicz83@gmail.com>
2019-02-14 06:50:50 +0100

    Rozwiazanie zadania 1

--- src/main/java/chapter1/task1_1/Main.java
+++ src/main/java/chapter1/task1_1/Main.java
@@ -1,10 +1,25 @@
 package chapter1.task1_1;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+/**
+ * Napisz program, który oblicza pole prostokąta. Wartość boków a i b wprowadzamy z klawiatury
+ * W programie należy przyjąć, że zmienne a, b oraz pole są typu double(rzeczywistego)
+ */
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("tast1");
-        System.out.println("tast1");
+        double a, b, pole;
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Program obicza pole prostokąta z podanych boków.");
+        System.out.print("Podaj długośc boku a: ");
+        a = scaner.nextDouble();
+        System.out.print("Podaj długośc boku b: ");
+        b = scaner.nextDouble();
+        pole = a*b;
+        System.out.println("Pole prostokąta o bokach "+ a + " i " + b + " wynosi: " + pole);
     }
 }
